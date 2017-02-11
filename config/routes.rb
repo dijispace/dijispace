c7cee3409e81ee8d5e891a15172019ed38f664aa
Worlanyo Damalie <worlalidamalie@gmail.com>
2017-02-11 13:21:58 +0000

    Basic Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   #get '/listings', to: 'listings#index'
   root 'listings#index'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create' 
   resources  :listings
+  resources  :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209131949) do
+ActiveRecord::Schema.define(version: 20170209232320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170209131949) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
