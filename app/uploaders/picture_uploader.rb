ed544ceccb69a96f51cc32df0a3abaf29089374c
Worlanyo Damalie <worlalidamalie@gmail.com>
2017-02-27 03:54:48 +0000

    Set up cloudinary for image pload on heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,24 +1,24 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-  #include Cloudinary::CarrierWave
+  include Cloudinary::CarrierWave
   # Include RMagick or MiniMagick support:
    #include CarrierWave::RMagick
    include CarrierWave::MiniMagick
    process resize_to_limit: [330,350]
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
-   if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #  if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+   # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -38,9 +38,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+   # version :thumb do
+   #   process :resize_to_fit => [50, 50]
+   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
